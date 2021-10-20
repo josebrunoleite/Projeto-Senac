e51ae893f0d78c13eccf31ac6593722b33816983
Umarekawaru <60050746+Umarekawaru@users.noreply.github.com>
2021-10-20 13:22:31 -0300

    Ver. 0.5
    Navbar/Slide/Sobre Mim/ Menu foram implmentamentos.

---
+++ body/extra.php
@@ -0,0 +1,10 @@
+	<!--Fonte-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Rammetto+One&display=swap" rel="stylesheet">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+	<!-- Favicons -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
---
+++ body/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta >
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="image.css">
+	<title>Test-kun</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php include('extra.php');?>
+
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg">
+	  <div class="container-fluid container">
+	    <a class="navbar-brand" href="#"><img id="logo" src="../images/header/logo.png" alt="Logo"></a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" style="justify-content: flex-end;" id="navbarNav">
+	      <ul class="navbar-nav">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#">Home</a>
+	        </li>
+	        <li class="nav-item d-flex">
+	          <a class="nav-link" href="#">Home</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Sobre-Mim</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Bolos</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Almoços</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Fale comigo</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+	<!--INICIO IMAGEM-->
+	<div id="imag-up">
+		<div class="title">
+			<h2 id="Title-Ima">IMAGEM ESTATICA COM O NOME E A LOGO NO MEIO</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+	</div>
+	<!--MEIO-->
+	<section class="info">
+		<div class="clearfix  container">
+		  	<img src="../images/sobre-mim/nome.png" class="col-md-3 float-start mb-3 ms-md-3" alt="...">
+		  	<div class="text-center info-text">
+			  <h1>
+			    Sobre mim.
+			  </h1>
+
+			  <p>
+			    As you can see the paragraphs gracefully wrap around the floated image. Now imagine how this would look with some actual content in here, rather than just this boring placeholder text that goes on and on, but actually conveys no tangible information at. It simply takes up space and should not really be read.
+			  </p>
+
+			  <p>
+			    And yet, here you are, still persevering in reading this placeholder text, hoping for some more insights, or some hidden easter egg of content. A joke, perhaps. Unfortunately, there's none of that here.
+			  </p>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="card-group">
+		  <div class="card">
+		    <img src="../images/card/card-1.png" class="card-img-top " alt="...">
+		    <div class="card-body">
+		      <h1 class="card-title text-center">Bolos</h1>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <img src="../images/card/card-2.png" class="card-img-top " alt="...">
+		    <div class="card-body">
+		      <h1 class="card-title text-center">Especiais</h1>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <img src="../images/card/card-3.png" class="card-img-top" alt="...">
+		    <div class="card-body">
+		      <h1 class="card-title text-center">Gourmet</h1>
+		    </div>
+		  </div>
+		</div>
+		<!-- Teste... <div class="card-group flex-center container">
+			<div class=" titulo">
+				<h1 style="font-size: 24px;">GOURMET</h1>
+			</div>
+			<div class="titulo">
+				<h1>NANANA</h1>
+			</div>
+			<div class="titulo">
+				<h1>AHAHAHA</h1>
+			</div>-->
+		</div>
+	</section>
+	<section>
+		<div class="card-group container">
+			<div class="card text-black">
+			  <img src="card-3.png" class="card-img" alt="...">
+			  <div class="card-img-overlay">
+			    <h5 class="card-title">Card title</h5>
+
+			  </div>
+			</div>
+			<div class="card text-black">
+			  <img src="card-3.png" class="card-img" alt="...">
+			  <div class="card-img-overlay">
+			    <h5 class="card-title">Card title</h5>
+
+			  </div>
+			</div>
+			<div class="card text-black">
+			  <img src="card-3.pnglass="card-img" alt="...">
+			  <div class="card-img-overlay">
+			    <h5 class="card-title">Card title</h5>
+			  </div>
+			</div>
+		</div>
+	</section>
+	<!-- Aqui Começa o Instragram
+	<section>
+		<div class="row row-cols-1 row-cols-md-2 g-1">
+		  <div class="col">
+		    <div class="card h-100">
+		      <img src="card-3.png" class="card-img-top" alt="...">
+		      <div class="card-body">
+		        <h5 class="card-title">Card title</h5>
+		        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col">
+		    <div class="card h-100">
+		      <img src="card-3.png" class="card-img-top" alt="...">
+		      <div class="card-body">
+		        <h5 class="card-title">Card title</h5>
+		        <p class="card-text">This is a short card.</p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col">
+		    <div class="card h-50">
+		      <img src="card-3.png" class="card-img-top" alt="...">
+		      <div class="card-body">
+		        <h5 class="card-title">Card title</h5>
+		        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col">
+		    <div class="card h-100">
+		      <img src="card-3.png" class="card-img-top" alt="...">
+		      <div class="card-body">
+		        <h5 class="card-title">Card title</h5>
+		        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</section>
+	Aqui acaba o Instragram-->
+	<div class="border-top">
+		<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+		    <p class="col-md-4 mb-0 text-muted font-fot">&copy; 2021 Company, Inc</p>
+
+		    <ul class="nav col-md-6 justify-content-end">
+		      <li class="nav-item font-fot font-fot"><a href="#" class="nav-link px-5 text-muted">Home</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Features</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Pricing</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">FAQs</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">About</a></li>
+		      <li class="ms-3"><a class=" bi bi-instagram text-muted" style="" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+		      <li class="ms-3"><a class="bi bi-whatsapp text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+		    </ul>
+		  </footer>
+		</div>
+
+	<div class="b-example-divider"></div>
+</body>
+</html>

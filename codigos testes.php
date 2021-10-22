76a030e319a4641bc080769868b05fb9de8cb597
Umarekawaru <60050746+Umarekawaru@users.noreply.github.com>
2021-10-22 16:54:39 -0300

    Ver. 0.6.0
    Refeito Footer, Repaginação no navbar e E RESPONSIVO IMPLEMENTADO!

---
+++ codigos testes.php
@@ -0,0 +1,17 @@
+<!--INICIO Footer-->
+	<div class="border-top">
+		<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+		    <p class="col-md-4 mb-0 text-muted font-fot">&copy; 2021 Company, Inc</p>
+
+		    <ul class="nav col-md-6 justify-content-end">
+		      <li class="nav-item font-fot font-fot"><a href="#" class="nav-link px-5 text-muted">Home</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Features</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Pricing</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">FAQs</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">About</a></li>
+		      <li class="ms-3"><a class=" bi bi-instagram text-muted" style="" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+		      <li class="ms-3"><a class="bi bi-whatsapp text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+		    </ul>
+		  </footer>
+		</div>
+	<div class="b-example-divider"></div>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid container">
+      <a class="navbar-brand" href="#"><img id="logo" src="images/header/logo.png" alt="Logo"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bi bi-text-center"></i>
+      </button>
+      <div class="collapse navbar-collapse" style="justify-content: flex-end;" id="navbarNav">
+        <ul class="navbar-nav">
+        
+            <a class="nav-link" href="#">Home</a>
+                 
+            <a class="nav-link" href="#">Sobre-Mim</a>
+         
+         
+            <a class="nav-link" href="#">Bolos</a>
+         
+          
+            <a class="nav-link" href="#">Almoços</a>
+          
+            <a class="nav-link" href="#">Fale comigo</a>
+         
+        </ul>
+      </div>
+    </div>
+  </nav>

b832f3f699d85937e969efb9c2a58874111ca75a
Umarekawaru <60050746+Umarekawaru@users.noreply.github.com>
2021-10-22 14:31:12 -0300

    Inicio PHP
    Adicionando pasta PHP

--- body/header.php
+++ body/header.php
@@ -1,5 +1,5 @@
 
- 	<link rel="shortcut icon" href="../images/header/cake-icon.png" />
+ 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--Fonte-->
 	<link rel="preconnect" href="https://fonts.googleapis.com">

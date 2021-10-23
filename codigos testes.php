d7581bf634933065f78c1fce818a65d34827a17c
LuigiTheLord <luigi.1.lhr@gmail.com>
2021-10-22 21:10:12 -0300

    mudanca-teste
    trocando features

--- codigos testes.php
+++ codigos testes.php
@@ -5,8 +5,8 @@
 
 		    <ul class="nav col-md-6 justify-content-end">
 		      <li class="nav-item font-fot font-fot"><a href="#" class="nav-link px-5 text-muted">Home</a></li>
-		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Features</a></li>
-		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Pricing</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">Featjoseleiteures</a></li>
+		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">paralelepipedo</a></li>
 		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">FAQs</a></li>
 		      <li class="nav-item font-fot"><a href="#" class="nav-link px-5 text-muted">About</a></li>
 		      <li class="ms-3"><a class=" bi bi-instagram text-muted" style="" href="#"><svg class="bi" width="24" height="24"></svg></a></li>

0e5b8ca6cfbd8fe0d7436ee7eeb0f8b4201d0b89
AtilaGoes <93164711+AtilaGoes@users.noreply.github.com>
2021-10-25 20:50:38 -0300

    Enviar php do Login

---
+++ admin/enviar.php
@@ -0,0 +1,16 @@
+<?php
+include_once('../config/connect.php');
+   $stmt = $conn->prepare("INSERT INTO users (id, name, login, password) VALUES(:ID, :NAME, :LOGIN, :PASSWORD)");
+
+   $id = $_POST['id'];
+   $name = $_POST['name'];
+   $login = $_POST['login'];
+   $password = $_POST['password'];
+
+   $stmt->bindParam(":ID", $id);
+   $stmt->bindParam(":NAME", $name);
+   $stmt->bindParam(":LOGIN", $login);
+   $stmt->bindParam(":PASSWORD", $password);
+   $stmt->execute();
+
+?>

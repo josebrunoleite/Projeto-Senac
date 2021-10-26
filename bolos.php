0d2f206e64d218629f20b889cfc960c61060b7e3
Daniel Marinho <marinhocosta.daniel@gmail.com>
2021-10-25 22:19:48 -0300

    Mudanças
    Adicionado bolos.php, pequeno ajuste no index.php em relação aos links, adicionado as imagens de card do bolos.php.

--- bolos.php
+++ bolos.php
@@ -7,5 +7,51 @@
 </head>
 <?php include_once('navbar.php'); ?>
 <body>
-    
+    <!-- Imagem Principal (Bolos) -->
+<section id="imag-up">
+		<div id="title-up">
+			<img id="title-ima" src="images/header/Reserva/marlucy gourmet.png" alt="Logo">
+		</div>
+	</section>
+    <!-- Cards -->
+    <!-- <linha1> -->
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col">
+            <div class="card">
+            <img src="images/instagram-cards/card1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Gourmet 1</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="images/instagram-cards/card2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Gourmet 2</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="images/instagram-cards/card3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Gourmet 3</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="images/instagram-cards/card4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Gourmet 4</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    <!-- </linha1>
     <?php include_once('footer.php'); ?>

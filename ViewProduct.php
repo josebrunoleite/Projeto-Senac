43e7bab321720a96402e9e71c81eee45f465b451
Daniel Marinho <marinhocosta.daniel@gmail.com>
2021-10-28 20:53:19 -0300

    view.php e viewproduct

---
+++ ViewProduct.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('../config/connect.php');
+
+$stmt = $conn->prepare("SELECT * FROM products ORDER BY id");
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+?>
+
+<div class="container">
+
+    <h1 id="main-title">Minhas Receitas</h1>
+
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Imagem</th>
+                </tr>
+            </thead>
+        </table>
+</div>
--- admin/view.php
+++ admin/view.php
@@ -7,16 +7,15 @@ $stmt->execute();
 
 $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
- foreach ($results as $row) {
+
+ ?>
+
+ <html>
+       
+   <?=foreach ($results as $row) {
+     
     foreach ($row as $key => $value) {
         echo "<strong>".$key."</strong>".$value."<br/>";
     }
- }
-
- ?>
- 
-<html>
-<body>
-<img src="<?=$post["image"]?>" width="80px" alt="">
-</body>
-</html>
+ }?>
+ </html>

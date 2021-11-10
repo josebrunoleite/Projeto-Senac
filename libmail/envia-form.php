e37a03ecd4c3b4efd1ca857bbc35d892aaf6921f
LuigiTheLord <luigi.1.lhr@gmail.com>
2021-11-09 21:12:23 -0300

    envia-form 3
    aaa

--- libmail/envia-form.php
+++ libmail/envia-form.php
@@ -3,7 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'lib/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 

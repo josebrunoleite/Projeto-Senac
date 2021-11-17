2243aa9a344685ea6e199f90bddfa7ef58391654
Umarekawaru <60050746+Umarekawaru@users.noreply.github.com>
2021-11-16 23:04:01 -0300

    Aula 16/11

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 text-white bg-dark pt-3 dark navbar-expand-lg">
-	<h2 style="font-size: 30px;">Bem vindo <?=$_SESSION['nome']?>!</h2>
+	<h2 style="font-size: 30px;">Bem vindo, <?=$_SESSION['nome']?>!</h2>
 
 
 	<button class="navbar-toggler dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="background-color: white;">
@@ -25,13 +25,13 @@
 		<li class="nav-item dark mt-1">
 			<a href="form-home.php">Atualizar Sobre Mim</a>
 		</li>
-		<form action="../config/deslogar.php" class="dark mt-1 ti">
 		<hr>
+		<form action="deslogar.php" class="dark mt-1 ti">
 		<li class="nav-item mb-2 dark">
 			<a href="deslogar.php" name="sair" value="1">Sair</a>
 		</li>
-		<hr>
 		</form>
+		<hr>
 	</ul>
 </div>
 </nav>
